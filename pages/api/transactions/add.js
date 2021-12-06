744452adefbf6aba7f7ce580865b2f98d890864d
Raghunath Prabhakar <82239949+Raghuboi@users.noreply.github.com>
2021-12-05 19:44:14 -0600

    Update API to fit with new model

--- pages/api/transactions/add.js
+++ pages/api/transactions/add.js
@@ -9,11 +9,12 @@ export default async function handler(req, res) {
 	switch (method) {
 		case 'POST':
 			try {
-				const { uid, amount, category } = req.body
+				const { uid, amount, category, currency } = req.body
 				const transaction = await new Transaction({
 					uid: uid,
 					amount: amount,
 					category: category,
+					currency: currency,
 				}).save()
 				res.status(201).json({ success: true, transaction: transaction })
 			} catch (error) {
