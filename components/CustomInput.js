06676eaa5e7b1482889b8c2a10260217430bbf44
Raghunath Prabhakar <82239949+Raghuboi@users.noreply.github.com>
2021-12-05 19:43:58 -0600

    Add currency to transaction input

---
+++ components/CustomInput.js
@@ -0,0 +1,79 @@
+import { Field } from 'formik'
+import {
+	Text,
+	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	Input,
+	InputGroup,
+	InputLeftAddon,
+	Button,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+} from '@chakra-ui/react'
+import { ChevronDownIcon } from '@chakra-ui/icons'
+
+export function CustomInputField({ type, label, name, error, touched }) {
+	return (
+		<Field type={type} name={name}>
+			{({ field }) => (
+				<FormControl isInvalid={error && touched}>
+					<FormLabel htmlFor={name}>{label}</FormLabel>
+					<Input {...field} type={type} id={name} />
+					<FormErrorMessage>{error}</FormErrorMessage>
+				</FormControl>
+			)}
+		</Field>
+	)
+}
+
+export function CurrencyInputField({
+	type,
+	label,
+	name,
+	error,
+	touched,
+	setCurrency,
+	currency,
+}) {
+	const CurrencyMenu = () => {
+		return (
+			<Menu>
+				<MenuButton
+					as={Button}
+					borderRadius='inherit'
+					_focus={{ outline: 'none' }}
+					rightIcon={<ChevronDownIcon ml={2} />}
+				>
+					{currency}
+				</MenuButton>
+				<MenuList w='max-content'>
+					<MenuItem onClick={() => setCurrency('$')}>$</MenuItem>
+					<MenuItem onClick={() => setCurrency('₹')}>₹</MenuItem>
+					<MenuItem onClick={() => setCurrency('£')}>£</MenuItem>
+					<MenuItem onClick={() => setCurrency('€')}>€</MenuItem>
+				</MenuList>
+			</Menu>
+		)
+	}
+
+	return (
+		<Field type={type} name={name}>
+			{({ field }) => (
+				<FormControl isInvalid={error && touched}>
+					<FormLabel htmlFor={name}>{label}</FormLabel>
+					<InputGroup>
+						{/*eslint-disable-next-line react/no-children-prop*/}
+						<InputLeftAddon w='fit-content' p={0}>
+							<CurrencyMenu />
+						</InputLeftAddon>
+						<Input {...field} type={type} id={name} />
+					</InputGroup>
+					<FormErrorMessage>{error}</FormErrorMessage>
+				</FormControl>
+			)}
+		</Field>
+	)
+}
--- components/TransactionModal.js
+++ components/TransactionModal.js
@@ -1,6 +1,7 @@
+import { useState } from 'react'
 import * as Yup from 'yup'
 import { Formik, Form } from 'formik'
-import CustomInputField from './CustomInputField'
+import { CurrencyInputField, CustomInputField } from './CustomInput'
 import {
 	Flex,
 	Button,
@@ -17,16 +18,68 @@ import {
 } from '@chakra-ui/react'
 import { AddIcon } from '@chakra-ui/icons'
 
-const initialValues = { amount: null, category: '' }
+const initialValues = { amount: 50, category: 'Groceries' }
 const validationSchema = Yup.object().shape({
-	category: Yup.string().required('Category is Required'),
+	category: Yup.string().required('Category is Required').max(15, 'Too long'),
 	amount: Yup.number()
 		.required('Amount is Required')
+		.moreThan(0, 'Cannot be less than 1')
 		.typeError('Invalid Amount'),
 })
 
 const TransactionModal = ({ onSubmit }) => {
 	const { isOpen, onOpen, onClose } = useDisclosure()
+	const [currency, setCurrency] = useState('$') // default currency
+
+	const TransactionForm = () => {
+		return (
+			<Formik
+				initialValues={initialValues}
+				validationSchema={validationSchema}
+				onSubmit={async ({ amount, category }, actions) => {
+					await onSubmit(amount, category, currency)
+					actions.setSubmitting(false)
+					onClose()
+				}}
+			>
+				{({ errors, touched, isSubmitting, isValid }) => (
+					<Form>
+						<VStack justify='center' align='center' gridGap={1}>
+							<CurrencyInputField
+								name='amount'
+								label='Amount'
+								error={errors.amount}
+								touched={touched.amount}
+								currency={currency}
+								setCurrency={(currency) => {
+									setCurrency(currency)
+								}}
+							/>
+							<CustomInputField
+								name='category'
+								label='Category'
+								error={errors.category}
+								touched={touched.category}
+							/>
+							<Spacer />
+							<Button
+								isLoading={isSubmitting}
+								isDisabled={!isValid}
+								type='submit'
+								colorScheme='green'
+								size='md'
+								w={120}
+								variant='solid'
+							>
+								Add
+							</Button>
+						</VStack>
+					</Form>
+				)}
+			</Formik>
+		)
+	}
+
 	return (
 		<>
 			<Button colorScheme='green' onClick={onOpen}>
@@ -44,46 +97,7 @@ const TransactionModal = ({ onSubmit }) => {
 					<ModalCloseButton />
 					<ModalBody>
 						<Flex justify='center' align='center'>
-							<Formik
-								initialValues={initialValues}
-								validationSchema={validationSchema}
-								onSubmit={async ({ amount, category }, actions) => {
-									await onSubmit(amount, category)
-									actions.setSubmitting(false)
-									onClose()
-								}}
-							>
-								{({ errors, touched, isSubmitting, isValid }) => (
-									<Form>
-										<VStack justify='center' align='center' gridGap={1}>
-											<CustomInputField
-												name='amount'
-												label='Amount'
-												error={errors.amount}
-												touched={touched.amount}
-											/>
-											<CustomInputField
-												name='category'
-												label='Category'
-												error={errors.category}
-												touched={touched.category}
-											/>
-											<Spacer />
-											<Button
-												isLoading={isSubmitting}
-												isDisabled={!isValid}
-												type='submit'
-												colorScheme='green'
-												size='md'
-												w={120}
-												variant='solid'
-											>
-												Add
-											</Button>
-										</VStack>
-									</Form>
-								)}
-							</Formik>
+							<TransactionForm />
 						</Flex>
 					</ModalBody>
 					<ModalFooter></ModalFooter>
