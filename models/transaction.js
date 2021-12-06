b012c1e1dcc03c6f8db527d72bf1fea6beea1091
Raghunath Prabhakar <82239949+Raghuboi@users.noreply.github.com>
2021-12-05 19:43:29 -0600

    Update transaction model

--- models/transaction.js
+++ models/transaction.js
@@ -13,6 +13,16 @@ const transactionSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	isExpense: {
+		type: Boolean,
+		required: true,
+		default: true,
+	},
+	currency: {
+		type: 'String',
+		required: true,
+		default: '$',
+	},
 })
 
 export default mongoose.models.Transaction ||
